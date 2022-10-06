b410f1ed2d80ee79e29db2deedbc89ffa035a973
Rajm1505 <95242392+Rajm1505@users.noreply.github.com>
2022-10-06 22:31:24 +0530

    register api done

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 import jwt,datetime
-import requests
+import requests,xmltodict
 from requests.auth import HTTPBasicAuth
 
 
-from .models import User
+from .models import User, UserDetails
 from .Serializers import UserSerializer,UserDetailsSerializers
 from django.views.decorators.csrf import csrf_exempt
 
@@ -117,7 +117,7 @@ class SendOtpView(APIView):
             user.otp = OTP
             user.save()
             response.data = {
-            'message' : 'Success'
+            'message' : 1
             }
             payload = {
                 'email' : email,
@@ -136,7 +136,7 @@ class SendOtpView(APIView):
 
         else:
             response.data = {
-            'error':'Email cannot be sent!'            
+            'message' : 0
             }
             return response
 
@@ -184,48 +184,6 @@ class VerifyOtpView(APIView):
             return response
 
 
-
-class LoginView(APIView):
-    def post(self,request):
-        print(request.data)
-        email = request.data['email']
-        password = request.data['password']
-
-        user = User.objects.filter(email=email).first()
-        response = Response()
-
-        if user is None:
-            # raise AuthenticationFailed("User not found")
-            response.data = {'error':'User not found',"detail": "Unauthenticated"}
-            
-            print(response.data)
-            return response
-
-        if not user.check_password(password):
-            # raise AuthenticationFailed("Invalid password")
-            response.data = {'error':'Invalid password',"detail": "Unauthenticated"}
-            
-            print(response.data)
-            return response
-        
-        payload = {
-            'sid': user.sid,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
-        }
-        
-        token = jwt.encode(payload,'secret',algorithm='HS256')
-        
-        response.set_cookie(key='jwt',value=token,httponly=True)
-        
-        response.data = {
-            'jwt' : token,
-            'message' : 'Success'
-        }
-        print(response.data)
-        return response
-
-
 def getRefreshToken(request):
     if request.method == 'GET':
         email = isAuth(request).data['email']
@@ -252,6 +210,83 @@ def getRefreshToken(request):
         user.save()
                                                                                                                                                                                                                   
         return redirect('StuDoc')
+
+# def getAadhaarDetails(request):
+#     if request.method == 'GET':
+#         email = isAuth(request).data['email']
+#         user = User.objects.get(email = email)
+
+#         userdetails = UserDetails.objects.get(uid=user.uid)
+#         userdetails.uid = user
+    
+#         print("userrefresh",user.refreshtoken)
+
+#         url = 'https://api.digitallocker.gov.in/public/oauth2/1/token'
+#         myobj = {
+#             "refresh_token": user.refreshtoken,
+#             "grant_type": "refresh_token",
+#         }
+#         # API call for obtaining accesstoken
+    
+#         refreshtokencall = requests.post(url, json = myobj,auth = HTTPBasicAuth('2407FC9F', '69e83492f63f996bfd5d')).json()  
+#         accesstoken = refreshtokencall.get('access_token')
+#         refreshtoken = refreshtokencall.get('refresh_token')
+#         print("access: ",accesstoken)
+#         print("refresh_tokenL: ",refreshtoken)
+#         # open('hello.txt','wb').write(accesstoken)  
+#         print("refreshtokencall",refreshtokencall)
+#         user.refreshtoken = refreshtoken
+#         user.save()
+
+#         # API call for obtaining list of files in user's digilocker
+        
+
+#         filelist = requests.get('https://api.digitallocker.gov.in/public/oauth2/2/files/issued',headers = {"Authorization": "bearer " + accesstoken}).json()
+#         print(filelist)
+
+#         # Extracting uris of required files from the file list
+
+#         requiredfiles = ['Class X Marksheet','Aadhaar Card','Income Certificate','Creamy - Non Creamy Layer Application','Ration Card']
+#         # category/caste certificate, 12th marksheet, self photo, self signature, permanent address proof, permanent address proof, disability certificate(if required)
+#         fileuris = []
+#         filenames = []
+#         for i in range(len(filelist['items'])):
+#                 if(filelist['items'][i]['name'] == 'Aadhaar Card'):
+#                     fileuris.append(filelist['items'][i]['uri'])
+#                     filenames.append(filelist['items'][i]['name'])
+
+#         print(fileuris) 
+#         # #Files which are not uploaded in users digilocker account
+#         # resdict = {}
+#         # for i in requiredfiles:
+#         #     if i not in filenames:
+#         #         resdict[i] = False
+#         #     else:
+#         #         resdict[i] = True
+#         # print(resdict)
+
+#     # Requesting XML of files from digilocker 
+    
+#         for fileuri in fileuris:
+#             if 'ADHAR' in fileuri:
+#                 xml = requests.get("https://api.digitallocker.gov.in/public/oauth2/1/xml/" + fileuri,headers = {"Authorization": "bearer " + accesstoken})
+#                 content  = xmltodict.parse(xml.content)
+#                 open('media/aadhaartest.xml','wb').write(xml.content)
+#                 studentdetails = StudentDetails.objects.get(sid=sid)
+#                 studoca.auid = content['KycRes']['UidData']['@uid'][8:]
+#                 studoca.aname = content['KycRes']['UidData']['Poi']['@name']
+#                 dob = content['KycRes']['UidData']['Poi']['@dob']
+#                 studoca.agender = content['KycRes']['UidData']['Poi']['@gender']
+#                 studoca.aaddress = content['KycRes']['UidData']['Poa']['@co'] + ' ' + content['KycRes']['UidData']['Poa']['@lm'] + ' ' + content['KycRes']['UidData']['Poa']['@loc'] + ' ' + content['KycRes']['UidData']['Poa']['@vtc']
+#                 temp = str(dob).split('-')[::-1]
+#                 studoca.adob =  "-".join(temp)
+#                 studoca.save()
+                
+#                 # Mandaviya Raj Jayesh
+#                 print(studoca.auid,studoca.aname,dob,studoca.agender)
+#     return redirect('StuDoc')
+
+
 
 def getFiles(request):
     if request.method == 'GET':
@@ -378,38 +413,6 @@ def getFiles(request):
         # return render(request, 'index.html',resdict)
         
         return redirect('StuDoc')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class UserView(APIView):
-    
-    def get(self,request):
-        token = request.COOKIES.get('jwt')
-        
-        if not token:
-            raise AuthenticationFailed('Unauthenticated')
-        
-        try:
-            payload = jwt.decode(token,'secret',algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated')
-            
-        user = User.objects.filter(sid = payload['sid']).first()
-        serializer = UserSerializer(user)
-        
-        return Response(serializer.data)
-    
     
 class LogoutView(APIView):
     def post(self,request):
